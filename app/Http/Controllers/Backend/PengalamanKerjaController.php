e1645feeda20eb73149e95458998ece7fda774d0
Farkhan04 <e41230747@polije.ac.id>
2025-03-03 10:54:33 +0700

    Acara14

--- app/Http/Controllers/Backend/PengalamanKerjaController.php
+++ app/Http/Controllers/Backend/PengalamanKerjaController.php
@@ -9,14 +9,18 @@ use Illuminate\Support\Facades\DB;
 
 class PengalamanKerjaController extends Controller
 {
-    public function index(){
-        $pengalaman_kerja =DB::table('pengalaman_kerja')->get();
+    public function index()
+    {
+        $pengalaman_kerja = DB::table('pengalaman_kerja')->get();
         return view('backend.pengalaman_kerja.index', compact('pengalaman_kerja'));
     }
-    public function create(){
+
+    public function create()
+    {
         $pengalaman_kerja = null;
-        return view('backend.pengalaman_kerja.create', compact('pengalaman_kerja')); 
+        return view('backend.pengalaman_kerja.create', compact('pengalaman_kerja'));
     }
+
     public function store(Request $request)
     {
         DB::table('pengalaman_kerja')->insert([
@@ -27,6 +31,33 @@ class PengalamanKerjaController extends Controller
         ]);
 
         return redirect()->route('pengalaman_kerja.index')
-        ->with('succes', 'Data pengalaman_kerja baru telah berhasil disimpan.');
+            ->with('succes', 'Data pengalaman_kerja baru telah berhasil disimpan.');
+    }
+
+    public function edit($id)
+    {
+        $pengalaman_kerja = DB::table('pengalaman_kerja')->where('id', $id)->first();
+        return view('backend.pengalaman_kerja.create', compact('pengalaman_kerja'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        DB::table('pengalaman_kerja')->where('id', $id)->update([
+            'nama' => $request->nama,
+            'jabatan' => $request->jabatan,
+            'tahun_masuk' => $request->tahun_masuk,
+            'tahun_keluar' => $request->tahun_keluar
+        ]);
+
+        return redirect()->route('pengalaman_kerja.index')
+            ->with('success', 'Pengalaman Kerja berhasil diperbaharui.');
+    }
+
+    public function destroy($id)
+    {
+        DB::table('pengalaman_kerja')->where('id', $id)->delete();
+
+        return redirect()->route('pengalaman_kerja.index')
+            ->with('success', 'Data Pengalaman Kerja berhasil dihapus');
     }
 }
