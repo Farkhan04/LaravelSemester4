ea5e65b15055f8ad64c90c667713f2fb85eab908
Farkhan04 <e41230747@polije.ac.id>
2025-03-08 12:43:51 +0700

    Acara22 (selesai)

--- app/Http/Controllers/Backend/ApiPendidikanController.php
+++ app/Http/Controllers/Backend/ApiPendidikanController.php
@@ -17,4 +17,40 @@ class ApiPendidikanController extends Controller
         // Mengembalikan response JSON dengan status code 200 (OK)
         return Response::json($pendidikan, 200);
     }
+
+    public function getPen($id)
+    {
+        $pendidikan = Pendidikan::find($id);
+        return Response::json($pendidikan, 200);
+    }
+
+    public function createPen(Request $request)
+    {
+        Pendidikan::create($request->all());
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil ditambahkan!'
+        ], 201);
+    }
+
+    public function updatePen($id, Request $request)
+    {
+        Pendidikan::find($id)->update($request->all());
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil dirubah!'
+        ], 201);
+    }
+
+    public function deletePen($id)
+    {
+        Pendidikan::destroy($id);
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil dihapus!'
+        ], 201);
+    }
 }
